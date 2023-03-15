03b73942dac5d214570c5161bd20098beb7c8e52
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-03-15 18:37:28 +0100

    Added navbar to the app component

--- algolizer-frontend/src/app/app.module.ts
+++ algolizer-frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ algolizer-frontend/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit{
+  applicationName: string = "Algolizer ETF";
+  version: string = "1.0.0";
+
+  constructor(
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    
+  }
+
+  showStatistics() {
+    this.router.navigate(['./statistics']);
+  }
+}
